a2ed8284965024ec0cc284409f13d8d22f5eac45
31daylee <31daylee@gmail.com>
2024-01-29 11:43:08 +0900

    mapper 설정 완료

---
+++ src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
@@ -0,0 +1,21 @@
+package com.tenco.bank.repository.interfaces;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tenco.bank.repository.entity.User;
+
+// interface + xml 연결
+@Mapper
+public interface UserRepository {
+	public int insert(User user); 
+	public int updateById(User user);
+	public int deleteById(Integer id);
+	
+	// 단일 행으로 나오기에 return 타입은 User
+	public User findById(Integer id);
+	public List<User> findAll();
+	
+	
+}
